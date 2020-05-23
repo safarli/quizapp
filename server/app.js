d2621f9d63d64c3e9cb21a6ff2add3d129d126c5
Bendali Safarli <bendalisafarli@Bendalis-MacBook-Air.local>
2020-05-24 01:01:04 +0400

    setting up nodjs express server

---
+++ server/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT;
+
+app.use();
+
+app.get();
+
+app.listen(PORT, () =>
+  console.log(`Server launched and listening on port ${PORT}`)
+);
